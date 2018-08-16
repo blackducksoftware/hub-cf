a0e46b23e4da10da6cd7e03ab2e9c24227550bc4
Jason Fisher <jfisher@blackducksoftware.com>
2018-08-16 11:40:45 -0400

    Add missing bean dependencies

---
+++ black-duck-perceiver/src/main/java/com/blackducksoftware/integration/cloudfoundry/perceiver/EventMonitorServiceConfiguration.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2018 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.cloudfoundry.perceiver;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author fisherj
+ *
+ */
+@Configuration
+public class EventMonitorServiceConfiguration {
+    @Bean
+    protected CloseableHttpClient httpClient() {
+        return HttpClients.createDefault();
+    }
+
+    @Bean
+    protected HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(httpClient());
+        return clientHttpRequestFactory;
+    }
+
+    @Bean
+    public RestTemplate perceptorRestTemplate() {
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+}
